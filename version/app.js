99d618f711e1dcdc9cf1426640b5d0c53f416723
limtsesh <limtsesh@yahoo.co.uk>
2026-01-14 12:24:40 +0800

    Updated to print instead of console

--- version/app.js
+++ version/app.js
@@ -8,13 +8,15 @@ app.get('/', function (req, res) {
     // response = 'This is version 1 of the app.' + '\n';
     // response = 'This is version 2 of the app.' + '\n';
     // response = 'This is version 3.1 of the app.' + '\n';
-    response = 'This is version 4.0 of the app.' + '\n';
+    // response = 'This is version 4.0 of the app.' + '\n';
 
     const a = 'Mg==';
     const b = 'MTA=';
     const a_n = Number(Buffer.from(a, 'base64').toString('ascii'));
     const b_n = Number(Buffer.from(b, 'base64').toString('ascii'));
-    console.log(Math.pow(a_n,b_n));
+    // console.log(Math.pow(a_n,b_n));
+    response = 'This is version 4.1 of the app.' + '\n' + 'The answer to the quis is: ' + Math.pow(a_n,b_n) + '\n';
+
 
     //send the response to the client
     res.send(response);
